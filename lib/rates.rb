64cea5022e9602606f9233bbecd97aa54fb84083
sashman <sashman90@gmail.com>
2019-03-11 19:30:00 +0000

    Extracting currency source to be respnsible for data access alone

--- lib/rates.rb
+++ lib/rates.rb
@@ -7,21 +7,21 @@ class Rates
 
   def date?(date)
     string_date = date.strftime('%F')
-    @daily_rates.key?(string_date)
+    @daily_rates.date?(string_date)
   end
 
   def rate_on_date?(currency, date)
     return true if currency == @base_currency
 
     string_date = date.strftime('%F')
-    date?(date) && @daily_rates[string_date].key?(currency)
+    date?(date) && @daily_rates.rate_on_date?(currency, string_date)
   end
 
   def rate_on_date(currency, date)
     return 1 if currency == @base_currency
 
     string_date = date.strftime('%F')
-    @daily_rates[string_date][currency]
+    @daily_rates.rate_on_date(currency, string_date)
   end
 
 end
--- lib/rates_source.rb
+++ lib/rates_source.rb
@@ -1,11 +1,25 @@
-module RatesSource
+class RatesSource
   require 'json'
 
-  def self.from(source_config)
-    JSON.parse(read_exchange_source(source_config))
+  def initialize(source_config)
+    @data = JSON.parse(read_exchange_source(source_config))
   end
 
-  def self.read_exchange_source(source_config)
+  def rate_on_date(currency, date)
+    @data[date][currency]
+  end
+
+  def date?(date)
+    @data.key?(date)
+  end
+
+  def rate_on_date?(currency, date)
+    @data.key?(date) && @data[date].key?(currency)
+  end
+
+  private
+
+  def read_exchange_source(source_config)
     open(source_config[:file]).read
   end
 end
