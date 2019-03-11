2c8e672ee39ec0663c6244d5ad7717e99b6924b5
sashman <sashman90@gmail.com>
2019-03-11 19:31:41 +0000

    Removing unused validation menthods

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,16 +1,4 @@
 module Validation
-  def self.validate_date(string_date, exchange_rates)
-    unless exchange_rates.key?(string_date)
-      raise ArgumentError, "Requested date (#{string_date}) not available"
-    end
-  end
-
-  def self.validate_currency(currency, base_currency, exchange_rates)
-    unless exchange_rates.key?(currency) || (currency == base_currency)
-      raise ArgumentError, "Requested currency (#{currency}) not available"
-    end
-  end
-
   def self.date_present?(rates, date)
     unless rates.date?(date)
       raise ArgumentError, "Requested date (#{date}) not available"
