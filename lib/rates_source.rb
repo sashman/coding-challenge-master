4ade4e1b46e9e9be98cde1ac8dac05253de0540e
sashman <sashman90@gmail.com>
2019-03-12 00:22:45 +0000

    Passing tests with postgres source

--- lib/rates_source.rb
+++ lib/rates_source.rb
@@ -1,8 +1,10 @@
 module RatesSource
   require 'rates_source/json_file'
+  require 'rates_source/postgres'
 
   def self.from(source_config)
-    JsonFile.new(source_config)
+    # JsonFile.new(source_config)
+    Postgres.new(source_config)
   end
 
 end
--- lib/rates_source/postgres.rb
+++ lib/rates_source/postgres.rb
@@ -2,19 +2,25 @@ class Postgres
   require 'pg'
 
   def initialize(source_config)
+    @rate_query = 'SELECT rate FROM exchange_rates WHERE currency = $1 AND date = $2'
+    @date_query = 'SELECT rate FROM exchange_rates WHERE date = $1'
+
     @conn = PG.connect(source_config)
   end
 
   def rate_on_date(currency, date)
-    1
+    result = @conn.exec_params(@rate_query, [currency, date])
+
+    result[0]['rate']
   end
 
   def date?(date)
-    true
+    result = @conn.exec_params(@date_query, [date])
+    !result.num_tuples.zero?
   end
 
   def rate_on_date?(currency, date)
-    true
+    result = @conn.exec_params(@rate_query, [currency, date])
+    !result.num_tuples.zero?
   end
-
 end
